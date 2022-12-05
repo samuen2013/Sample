76ba08d572e11b7cc0e956084b10bd369f39834e
曹盛淵 <max.tsao@vivotek.com>
2022-12-05 16:09:59 +0800

    Initial Commit

---
+++ test/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  test
+//
+//  Created by 曹盛淵 on 2022/12/5.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by 曹盛淵 on 2022/12/5.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
